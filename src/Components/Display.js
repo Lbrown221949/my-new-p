57244b178454283e31c253cc26798063c4559a2e
linda <lbrown22@gmail.com>
2021-07-14 13:32:23 -0500

    nessage

---
+++ src/Components/Display.js
@@ -0,0 +1,21 @@
+function Display() {
+  //eslint-disable-next-line  
+  let name="Linda Brown"
+  //eslint-disable-next-line
+    let skill="fed"
+    
+    return (
+      
+      <div>
+    
+      My name is {"name"}and my skill is {"fed"};
+     <Display text={name}/>
+     <Display text= {"My name is Linda Brown"}/>
+     <Display text= {"I am a Front-End-Developer"}/>
+       
+      </div> 
+    )
+
+
+ 
+    }
